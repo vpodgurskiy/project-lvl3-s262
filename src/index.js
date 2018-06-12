810d4c736c99794919c5e5a5b5084becb4e4bd6f
vpodgurskiy <vpodgurskiy@gmail.com>
2018-06-12 20:36:25 +0300

    fix path and fix catch err in pageLoader

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import axios from 'axios';
 import fs from 'mz/fs';
 import url from 'url';
 import os from 'os';
-import Path from 'path';
+import path from 'path';
 
 const getFileName = (uri) => {
   const { hostname, pathname } = url.parse(uri);
   return `${hostname.split('.').join('-')}${pathname.split('/').join('-')}.html`;
 };
 
-const getFullPath = (path, fileName) => {
-  const fullPath = Path.resolve(path, fileName);
+const getFullPath = (pathDestination, fileName) => {
+  const fullPath = path.resolve(pathDestination, fileName);
   return fullPath;
 };
 
-export default (uri, path = os.tmpdir()) => {
+export default (uri, pathDestination = os.tmpdir()) => {
   const fileName = getFileName(uri);
-  const fullPath = getFullPath(path, fileName);
+  const fullPath = getFullPath(pathDestination, fileName);
   return axios.get(uri)
     .then(response => fs.writeFile(fullPath, response.data, 'utf-8'))
-    .catch(err => console.log(err));
+    .catch((err) => {
+      throw err;
+    });
 };
